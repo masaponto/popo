df94cb1a075d1668369d41dd5701e48dc2e15072
Shunsuke Mie <sux2mfgj@gmail.com>
2014-11-09 15:23:12 +0900

    add lexical_analyser class

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -1,9 +1,59 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
 namespace popo {
 
-namespace lexer {
+    namespace lexer {
 
-}  // namespace lexer
+
+//         enum struct Token{
+//             variable
+//             list
+//             list,
+
+//             atom
+//             num,
+//             string,
+//             symbol,
+//             nil,
+
+//             left = '(',     // 40
+//             right = ')',    // 41
+
+//         }
+
+//         struct Lex{
+//             Token token,
+
+//             union Value {
+//                 std::string str;
+//                 int num;
+//                 std::string symbol;
+
+//             }
+//         };
+
+        class lexical_analyser
+        {
+            public:
+                lexical_analyser(const std::string &filename) :ifs(filename)
+                {
+                    assert(ifs);
+                }
+
+                ~lexical_analyser()
+                {
+                    ifs.close();
+                }
+
+            private:
+                std::ifstream ifs;
+
+        };
+
+    }  // namespace lexer
 }  // namespace popo
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "lexer.hpp"
 
 
 int main()
 {
+    using namespace popo;
 
+    std::string filename("test.scm");
+    lexer::lexical_analyser lex(filename);
 }
