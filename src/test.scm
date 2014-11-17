617cd756280feb50bbd93f4609d752e1d78a9289
himaaaatti <sux2mfgj@gmail.com>
2014-11-17 19:49:33 +0900

    add comment

--- src/test.scm
+++ src/test.scm
@@ -1,2 +1,27 @@
 (define (bar x)
     (foo 0 x))
+
+(let binds body)
+
+(define (k+ a b k)
+    (k (+ a b)))
+
+(define pi (* 4 (atan 1.0)))
+;    degree -> radian
+(define (radian deg)
+    (* deg (/ pi 180.0)))
+
+; free fall time
+(define (ff-time vy)
+    (/ (* 2.0 vy) 9.8))
+
+; horizontal distance
+(define (dx vx t)
+    (* vx t))
+
+; distance
+(define (distance v ang)
+    (dx
+        (* v (cos (radian ang)))                ; vx
+        (ff-time (* v (sin (radian ang))))))    ; t
+
