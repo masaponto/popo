8cc2d38fdbe65547bb401d74bc4e39b17beba434
masaponto <masaponto@gmail.com>
2015-01-10 19:47:03 +0900

    add syntax test

--- src/test/popo_test.cpp
+++ src/test/popo_test.cpp
@@ -3,103 +3,142 @@
 #include <syntax.hpp>
 #include <list>
 
-
 // lexical_analyser
 TEST(lexical_analyser, lex_test_1) {
-    using namespace popo::lexical;
-    std::string in_data("\
+  using namespace popo::lexical;
+  std::string in_data("\
             (+ 1 2)\
             ");
 
-    lexical_analyser<std::string> lex(in_data);
+  lexical_analyser<std::string> lex(in_data);
 
-    // (+ 1 2)
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::num, lex.get_next_token());
-    EXPECT_EQ(Token::num, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
-    EXPECT_EQ(Token::eof, lex.get_next_token());
-
+  // (+ 1 2)
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::num, lex.get_next_token());
+  EXPECT_EQ(Token::num, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
+  EXPECT_EQ(Token::eof, lex.get_next_token());
 }
 
-TEST(lexical_analyser, lex_test_2)
-{
-    using namespace popo::lexical;
-    std::string in_data("\
+TEST(lexical_analyser, lex_test_2) {
+  using namespace popo::lexical;
+  std::string in_data("\
             (define x 10)\
             ");
-    lexical_analyser<std::string> lex(in_data);
+  lexical_analyser<std::string> lex(in_data);
 
-    // (define x 10)
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::num, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
-    EXPECT_EQ(Token::eof, lex.get_next_token());
+  // (define x 10)
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::num, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
+  EXPECT_EQ(Token::eof, lex.get_next_token());
 }
 
-TEST(lexical_analyser, lex_test_3)
-{
-    using namespace popo::lexical;
-    std::string in_data("\
+TEST(lexical_analyser, lex_test_3) {
+  using namespace popo::lexical;
+  std::string in_data("\
             (define x \"hello\")\
             ");
-    lexical_analyser<std::string> lex(in_data);
+  lexical_analyser<std::string> lex(in_data);
 
-    // (define y "hello")
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::string, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
-    EXPECT_EQ(Token::eof, lex.get_next_token());
+  // (define y "hello")
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::string, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
+  EXPECT_EQ(Token::eof, lex.get_next_token());
 }
 
-TEST(lexical_analyser, lex_test_4)
-{
-    using namespace popo::lexical;
-    std::string in_data("\
+TEST(lexical_analyser, lex_test_4) {
+  using namespace popo::lexical;
+  std::string in_data("\
             (define z\
                 (lambda (a b)\
                     (+ a b)))\
             ");
-    lexical_analyser<std::string> lex(in_data);
+  lexical_analyser<std::string> lex(in_data);
 
-    // (define z
-    //      (lambda (a b)
-    //          (+ a b)))
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
-    EXPECT_EQ(Token::left, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::symbol, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
-    EXPECT_EQ(Token::right, lex.get_next_token());
+  // (define z
+  //      (lambda (a b)
+  //          (+ a b)))
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
+  EXPECT_EQ(Token::left, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::symbol, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
+  EXPECT_EQ(Token::right, lex.get_next_token());
 
-    EXPECT_EQ(Token::eof, lex.get_next_token());
+  EXPECT_EQ(Token::eof, lex.get_next_token());
 }
 
+
 // s_expression_parser
-TEST(syntax_analyzer, syntax_test_1)
-{
-    using namespace popo::syntax;
-    const std::string in_data(
-        "\
+TEST(syntax_analyzer, syntax_test_1) {
+  using namespace popo;
+  const std::string in_data("\
             (+ 1 2)\
             ");
-    s_expression_parser<std::string> parser(in_data);
-    auto expr_node = parser.s_exp_parse();
+  syntax::s_expression_parser<std::string> parser(in_data);
+  auto expr_node = parser.s_exp_parse();
+
+  auto cons_node = std::unique_ptr<syntax::cons_node>(
+      static_cast<syntax::cons_node *>(expr_node.release()));
+
+  auto car = std::move(cons_node->car);
+  EXPECT_EQ(syntax::node_type::symbol, car->type);
+
+  auto symbol_node = std::unique_ptr<syntax::symbol_node>(
+      static_cast<syntax::symbol_node *>(car.release()));
+
+  EXPECT_EQ(symbol_node->val, std::string("+"));
+
+  auto cdr = std::move(cons_node->cdr);
+  EXPECT_EQ(syntax::node_type::cons, cdr->type);
+
+  cons_node = std::unique_ptr<syntax::cons_node>(
+      static_cast<syntax::cons_node *>(cdr.release()));
+
+  EXPECT_EQ(syntax::node_type::num, cons_node->car->type);
+
+  car = std::move(cons_node->car);
+
+  auto num_node = std::unique_ptr<syntax::num_node>(
+      static_cast<syntax::num_node *>(car.release()));
+
+  EXPECT_EQ(num_node->val, 1);
+
+  cdr = std::move(cons_node->cdr);
+  EXPECT_EQ(syntax::node_type::cons, cdr->type);
+
+  cons_node = std::unique_ptr<syntax::cons_node>(
+      static_cast<syntax::cons_node *>(cdr.release()));
+
+  car = std::move(cons_node->car);
+  EXPECT_EQ(syntax::node_type::num, car->type);
+
+  num_node = std::unique_ptr<syntax::num_node>(
+      static_cast<syntax::num_node *>(car.release()));
+
+  EXPECT_EQ(num_node->val, 2);
+
+  cdr = std::move(cons_node->cdr);
+  EXPECT_EQ(syntax::node_type::nil, cdr->type);
+
+
 }
 
 // int main(int argc, char **argv) {
