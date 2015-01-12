107cdfa5536686199373b11caa9c2f805456e41d
himaaaatti <sux2mfgj@gmail.com>
2015-01-13 02:30:25 +0900

    add check code, whether directory is present

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 cd ./src/test
-wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
-unzip gtest-1.7.0.zip
+if [ ! -d gtest-1.7.0 ]; then
+    wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
+    unzip gtest-1.7.0.zip
+fi
 cd gtest-1.7.0
-mkdir build
+mkdir -p build
 cd build
 cmake ..
 make
 cd ../../
-mkdir build
+mkdir -p build
 cd build
 cmake ..
 make
-pwd
+# pwd
 ./popo_test
