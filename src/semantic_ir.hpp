5b1a67d4dcdf4786a814ffe64aa06606a0ddb0a6
himaaaatti <sux2mfgj@gmail.com>
2015-02-09 16:28:53 +0900

    a

---
+++ src/semantic_ir.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "syntax.hpp"
+#include "symbol_table.hpp"
+
+
+
+namespace popo {
+    namespace semantic {
+
+        template<typename Iteratable>
+        class semantic_analyzer {
+
+        public:
+            semantic_analyzer(const Iteratable& itr) {}
+
+
+        };
+
+    } // namespace semantic
+} // namespace popo
